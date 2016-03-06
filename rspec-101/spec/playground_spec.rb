d9ee94476a776a680316bd263cc501474784f71c
Gabriel Balbuena <gabrielbalbuena@gmail.com>
2016-03-06 17:46:41 -0300

    Rspec setup

---
+++ rspec-101/spec/playground_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../lib/playground'
+describe Playground do
+  context 'when there are no children' do
+    it 'is quite boring place' do
+    end
+    it 'is empty' do
+    end
+  end
+end
