d179b904729fb1afbf80e0e43b9c27ac46d2d113
Gabriel Balbuena <gabrielbalbuena@gmail.com>
2016-03-06 17:31:16 -0300

    Implements multiply on digits-calculator.rb

--- string-calculator.rb
+++ string-calculator.rb
@@ -11,6 +11,9 @@ class Calculator
   def sum
     digits.inject(:+)
   end
+  def multiply
+    digits.inject(:*)
+  end
   private
   def parse_input str
     str.split(',').map{ |n| n.to_i }
@@ -21,6 +24,11 @@ describe Calculator do
   it 'sums provided digits' do
     calculator = Calculator.new('1,2,3,4,5')
     calculator.sum.must_equal 15
+  end
+
+  it 'multiplies provided digits' do
+    calculator = Calculator.new('4,5')
+    calculator.multiply.must_equal 20
   end
 
 end
