732869a12f42e78b81ad61a4775680b3c02cb00a
Gabriel Balbuena <gabrielbalbuena@gmail.com>
2016-03-06 17:24:25 -0300

    Adds attr_reader to get the digits methods on place

--- string-calculator.rb
+++ string-calculator.rb
@@ -4,11 +4,12 @@ require 'bundler/setup'
 require 'minitest/autorun'
 
 class Calculator
+  attr_reader :digits
   def initialize(digits_string)
     @digits = digits_string.split(',').map{ |n| n.to_i }
   end
   def sum
-    @digits.inject(:+)
+    digits.inject(:+)
   end
 end
 
